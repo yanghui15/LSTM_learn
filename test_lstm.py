e530b5cf002777e519f1864a95d7cb37b25a47d2
yanghui15 <yanghui15@mails.tsinghua.edu.cn>
2017-01-21 14:23:22 +0800

    update lstm demo

--- test_lstm.py
+++ test_lstm.py
@@ -1,5 +1,86 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-dataset = pd.read_csv('international-airline-passengers.csv', usecols=[1], engine='python', skipfooter=3)
-plt.plot(dataset)
+import math
+import numpy as np
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import LSTM
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.metrics import mean_absolute_error
+
+df = pd.read_csv('international-airline-passengers.csv', usecols=[1], engine='python', skipfooter=3)
+
+print df.shape
+print df.head(5)
+#plt.plot(df)
+#plt.show()
+
+
+def create_dataset(dataset, look_back=1):
+	dataX, dataY = [], []
+	for i in range(len(dataset)-look_back-1):
+		a = dataset[i:(i+look_back), 0]
+		dataX.append(a)
+		dataY.append(dataset[i + look_back, 0])
+	return np.array(dataX), np.array(dataY)
+
+np.random.seed(2017)
+dataset = df.values
+dataset = dataset.astype('float32')
+
+# normalize the dataset
+scaler = MinMaxScaler(feature_range=(0, 1))
+dataset = scaler.fit_transform(dataset)
+
+# split into train and test sets
+train_size = int(len(dataset) * 0.67)
+test_size = len(dataset) - train_size
+train, test = dataset[0:train_size,:], dataset[train_size:len(dataset),:]
+
+# reshape into X=t and Y=t+1
+look_back = 3
+trainX, trainY = create_dataset(train, look_back)
+testX, testY = create_dataset(test, look_back)
+
+# reshape input to be [samples, time steps, features]
+trainX = np.reshape(trainX, (trainX.shape[0], trainX.shape[1], 1))
+testX = np.reshape(testX, (testX.shape[0], testX.shape[1], 1))
+
+# create and fit the LSTM network
+model = Sequential()
+model.add(LSTM(10, input_dim=1))
+model.add(Dense(1))
+model.compile(loss='mean_absolute_error', optimizer='adam')
+model.fit(trainX, trainY, nb_epoch = 100 , batch_size=1, verbose=2)
+
+# make predictions
+trainPredict = model.predict(trainX)
+testPredict = model.predict(testX)
+
+# invert predictions
+trainPredict = scaler.inverse_transform(trainPredict)
+trainY = scaler.inverse_transform([trainY])
+testPredict = scaler.inverse_transform(testPredict)
+testY = scaler.inverse_transform([testY])
+
+# calculate root mean squared error
+trainScore = (mean_absolute_error(trainY[0], trainPredict[:,0]))
+print('Train Score: %.2f MAE' % (trainScore))
+testScore = (mean_absolute_error(testY[0], testPredict[:,0]))
+print('Test Score: %.2f MAE' % (testScore))
+
+# shift train predictions for plotting
+trainPredictPlot = np.empty_like(dataset)
+trainPredictPlot[:, :] = np.nan
+trainPredictPlot[look_back:len(trainPredict)+look_back, :] = trainPredict
+
+# shift test predictions for plotting
+testPredictPlot = np.empty_like(dataset)
+testPredictPlot[:, :] = np.nan
+testPredictPlot[len(trainPredict)+(look_back*2)+1:len(dataset)-1, :] = testPredict
+
+# plot baseline and predictions
+plt.plot(scaler.inverse_transform(dataset))
+plt.plot(trainPredictPlot)
+plt.plot(testPredictPlot)
 plt.show()
